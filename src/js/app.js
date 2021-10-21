26fb6eee630b78a00617405bb6758c765a7ca9ab
Daniel <daniel_sh.99@abv.bg>
2021-10-21 23:46:50 +0300

    feat: fire fix in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,9 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+    document.querySelectorAll('.hot').forEach(elem => {
+        elem.classList.add("fas");
+        elem.classList.add("fa-fire");
+        elem.innerText+= '';
+    })
 });
